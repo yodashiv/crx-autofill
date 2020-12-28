a59439466ad30c12a1009b3627966252be380781
Shivam patel <yodashiv@gmail.com>
2020-12-28 12:58:34 -0800

    Autofill for linkedin working

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,9 +1,10 @@
 
 // Autofill values 
-const autoFillValues = (userInfo: userInfoI) => (
+const autoFillValues = (userInfo: userInfoI) => {
     //FIXME: allow for upload of resume
     let linkedinBox: HTMLElement = document.getElementsByName("urls[LinkedIn]")[0];
-    linkedinBox.innerHTML = "This is just a test";
-);
+    linkedinBox.setAttribute("autocomplete", "off");
+    linkedinBox.setAttribute("value", "This is just a test.");
+};
 
 chrome.storage.sync.get("userInfo", (result) => autoFillValues(result.userInfo));
